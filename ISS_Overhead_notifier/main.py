4b6e913bf77421cf0ece9c48d6b476c4f97de39c
sidarth-23 <sidarth157@gmail.com>
2023-08-02 15:46:40 +0530

    Completed till day 33

---
+++ ISS_Overhead_notifier/main.py
@@ -0,0 +1,54 @@
+import math
+import smtplib
+import requests
+from datetime import datetime
+import time
+
+MY_LAT = 11.016844
+MY_LONG = 76.955833
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_lat = float(data['iss_position']['latitude'])
+    iss_long = float(data['iss_position']['longitude'])
+
+    distance = math.sqrt(math.pow((MY_LAT - iss_lat), 2) + math.pow((MY_LONG - iss_long), 2))
+
+    if -5 < distance < 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lmg": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data['results']['sunrise'].split("T")[1].split(":")[0])
+    sunset = int(data['results']['sunset'].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+
+    if time_now.hour > sunset or time_now.hour < sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_night() and is_iss_overhead():
+        with open("userinfo.txt", mode="r") as file:
+            credentials = file.readlines()
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=credentials[0], password=credentials[1])
+            connection.sendmail(from_addr=credentials[0], to_addrs="sidarth158@gmail.com",
+                                msg="Subject:ISS Above you\n\nGo out and enjoy the view")
+            connection.close()
